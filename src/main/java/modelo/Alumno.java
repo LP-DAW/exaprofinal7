ff71249dc18d5dc2c5a73f34a1de73b3c9bd21b6
Profesor.asix <profesor.asix@gmail.com>
2016-05-09 17:04:13 +0200

    Model files cleaned

--- src/main/java/modelo/Alumno.java
+++ src/main/java/modelo/Alumno.java
@@ -12,55 +12,6 @@ package modelo;
  */
 public class Alumno {
 
-    private String ida;
-    private String noma;
-    private Grupo grupo;
 
-    public Alumno(String ida, String noma, Grupo grupo) {
-        throw new RuntimeException("Implementar");
-
-    }
-
-    /**
-     * @return the ida
-     */
-    public String getIda() {
-        return ida;
-    }
-
-    /**
-     * @param ida the ida to set
-     */
-    public void setIda(String ida) {
-        this.ida = ida;
-    }
-
-    /**
-     * @return the noma
-     */
-    public String getNoma() {
-        return noma;
-    }
-
-    /**
-     * @param noma the noma to set
-     */
-    public void setNoma(String noma) {
-        this.noma = noma;
-    }
-
-    /**
-     * @return the grupo
-     */
-    public Grupo getGrupo() {
-        return grupo;
-    }
-
-    /**
-     * @param grupo the grupo to set
-     */
-    public void setGrupo(Grupo grupo) {
-        this.grupo = grupo;
-    }
 
 }
--- src/main/java/modelo/Grupo.java
+++ src/main/java/modelo/Grupo.java
@@ -12,40 +12,5 @@ package modelo;
  */
 public class Grupo {
 
-    private String idg;
-    private String nomg;
-
-    public Grupo(String idg, String nomg) {
-        throw new RuntimeException("Implementar");
-
-    }
-
-    /**
-     * @return the idg
-     */
-    public String getIdg() {
-        return idg;
-    }
-
-    /**
-     * @param idg the idg to set
-     */
-    public void setIdg(String idg) {
-        this.idg = idg;
-    }
-
-    /**
-     * @return the nomg
-     */
-    public String getNomg() {
-        return nomg;
-    }
-
-    /**
-     * @param nomg the nomg to set
-     */
-    public void setNomg(String nomg) {
-        this.nomg = nomg;
-    }
 
 }
